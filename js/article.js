d3ed50994e0f1e810f7fe271ba3314835f8799da
grabbiel <fcruzado22@gmail.com>
2025-07-09 11:43:07 -0700

    readmore

--- js/article.js
+++ js/article.js
@@ -18,3 +18,14 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "/";
   }
 });
+
+document.addEventListener('click', function (e) {
+  const articleCard = e.target.closest('.article-card');
+  if (articleCard) {
+    e.preventDefault();
+    const readMoreLink = articleCard.querySelector('.read-more');
+    if (readMoreLink) {
+      readMoreLink.click();
+    }
+  }
+});
