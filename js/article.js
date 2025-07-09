61743ef3cd14fe207c9af858c6b1d1f4c3b3cde9
grabbiel <fcruzado22@gmail.com>
2025-07-09 11:50:08 -0700

    click not prevent

--- js/article.js
+++ js/article.js
@@ -2,7 +2,6 @@
 document.addEventListener('click', function (e) {
   const articleCard = e.target.closest('.article-card');
   if (articleCard) {
-    e.preventDefault();
     const readMoreLink = articleCard.querySelector('.read-more');
     if (readMoreLink) {
       readMoreLink.click();
