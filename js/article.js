d136c13fb208b53e18ace995ac84c1677b4f5069
grabbiel <fcruzado22@gmail.com>
2025-07-09 11:46:53 -0700

    laod js

--- js/article.js
+++ js/article.js
@@ -1,23 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Get article ID from URL (e.g., blog.grabbiel.com/123)
-  const articleId = window.location.pathname.split("/")[1];
-
-  if (articleId && !isNaN(articleId)) {
-    // Set the HTMX endpoint to load this specific article
-    const articleContainer = document.getElementById("article-container");
-    articleContainer.setAttribute(
-      "hx-get",
-      `https://server.grabbiel.com/article/${articleId}/index.html`,
-    );
-
-    // Trigger the load
-    htmx.trigger(articleContainer, "load");
-
-  } else {
-    // Not a valid article ID in the URL
-    window.location.href = "/";
-  }
-});
 
 document.addEventListener('click', function (e) {
   const articleCard = e.target.closest('.article-card');
